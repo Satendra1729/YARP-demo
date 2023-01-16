5dc6a3bd2f938735251f69f816fe1f88e38f006a
satendra1729 <satendra1729@gmail.com>
2023-01-16 22:42:16 +0530

    add request body logging by HTTP methods

--- Program.cs
+++ Program.cs
@@ -1,6 +1,38 @@
+
+var AllowedMethods =  new List<string>{"POST","PUT"}; 
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConsole();
+});
+
+builder.Configuration.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ProxySettings"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapReverseProxy(proxyPipeline =>
+{
+    proxyPipeline.Use(async (context, next) =>
+    {
+        if (AllowedMethods.Contains(context.Request.Method))
+        {
+            context.Request.EnableBuffering();
+            var bodyAsText = new StreamReader(context.Request.Body).ReadToEndAsync();
+            app.Logger.LogInformation("Body --> " + bodyAsText.Result);
+            context.Request.Body.Position = 0;
+        }
+        await next.Invoke(context);
+    });
+
+});
+
+app.Logger.LogInformation("Starting the app.......");
 
 app.Run();
