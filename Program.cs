9ce8e810a48d57875b80242c9bd957acd420e778
satendra1729 <satendra1729@gmail.com>
2023-01-19 17:38:54 +0530

    add config route surpress error and cleanup

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 
-var AllowedMethods =  new List<string>{"POST","PUT"}; 
+var AllowedMethods = new List<string> { "POST", "PUT" };
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,13 +9,22 @@ builder.Host.ConfigureLogging(logging =>
     logging.AddConsole();
 });
 
-builder.Configuration.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+builder.Configuration
+       .AddJsonFile("appsettings.json",true)
+       .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("YARP_ENVIRONMENT")}",true)
+       .AddEnvironmentVariables(prefix: "YARP_");
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ProxySettings"));
 
 var app = builder.Build();
 
+// Read target Enviorment appsettings using YARP_ENVIRONMENT, If not then read only appsettings.json
+// enabling the logging for POST and PUT request
+app.Map("/enable",() => "enabled"); 
+
+// disable the logging for POST and PUT request
+app.Map("/disable",() => "disabled"); 
 
 app.MapReverseProxy(proxyPipeline =>
 {
@@ -24,9 +33,22 @@ app.MapReverseProxy(proxyPipeline =>
         if (AllowedMethods.Contains(context.Request.Method))
         {
             context.Request.EnableBuffering();
-            var bodyAsText = new StreamReader(context.Request.Body).ReadToEndAsync();
-            app.Logger.LogInformation("Body --> " + bodyAsText.Result);
-            context.Request.Body.Position = 0;
+            // sepressing the Error 
+            try
+            {
+                var bodyAsText = new StreamReader(context.Request.Body).ReadToEndAsync();
+                app.Logger.LogInformation("Body --> " + bodyAsText.Result);
+            }
+            catch (Exception ex)
+            {
+                // Logging on Error
+                app.Logger.LogError(ex.Message);
+            }
+            finally
+            {
+                // Resetting the Postion of body streme to 0 regardless of error or normal scenario
+                context.Request.Body.Position = 0;
+            }
         }
         await next.Invoke(context);
     });
